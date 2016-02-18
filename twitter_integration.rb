1ec204257d1e6215c72a04f263c42208970ab115
Felipe <fchiapini@gmail.com>
2016-02-18 09:32:53 -0200

    Changed identation

--- twitter_integration.rb
+++ twitter_integration.rb
@@ -12,37 +12,37 @@ if !ARGV.empty?
     config.access_token_secret = "mSHLJ4elqyoajqTTsaUNW2xdetfYBkAXaxgWHhDt6LAIL"
   end
 
-	# One or multiple keywords
-	if ARGV.length == 1
-	  topic = ARGV[ 0 ]
-	else
-		topics = ARGV
-	end
+  # One or multiple keywords
+  if ARGV.length == 1
+    topic = ARGV[ 0 ]
+  else
+    topics = ARGV
+  end
 
-	puts ARGV.length
+  puts ARGV.length
 
-	# Filtering in the Twitter API by the keywords previously sent in the command line
-	tweets.filter( track: ARGV.length == 1 ? topic : topics.join( "," ) ) do | object |
+  # Filtering in the Twitter API by the keywords previously sent in the command line
+  tweets.filter( track: ARGV.length == 1 ? topic : topics.join( "," ) ) do | object |
 
-		# Building the tweet's parameters from the streaming Twitter API
-		params = { user_name: "@#{object.user.screen_name}", text: object.text, posted_at: object.created_at, twitter_id: object.id } if object.is_a?( Twitter::Tweet )
+  # Building the tweet's parameters from the streaming Twitter API
+  params = { user_name: "@#{object.user.screen_name}", text: object.text, posted_at: object.created_at, twitter_id: object.id } if object.is_a?( Twitter::Tweet )
 		
-		# Calling the post request to the API for storing tweets on the database
-		url = 'http://api.rails_twitter_api.dev/tweets'
-		uri = URI.parse( url )
+  # Calling the post request to the API for storing tweets on the database
+  url = 'http://api.rails_twitter_api.dev/tweets'
+  uri = URI.parse( url )
 
-		http = Net::HTTP.new( uri.host, uri.port )
+  http = Net::HTTP.new( uri.host, uri.port )
 
-		request = Net::HTTP::Post.new(
-		  uri.request_uri, 
-		 	initheader = {'Content-Type' =>'application/json'}
-		)
-		request.body = params.to_json
+  request = Net::HTTP::Post.new(
+                                 uri.request_uri, 
+                                 initheader = {'Content-Type' =>'application/json'}
+                               )
+  request.body = params.to_json
 
-		response = http.request( request )
-		puts( response.body )
+  response = http.request( request )
+  puts( response.body )
 
-	end
+  end
 else
 	puts "Fill with some topics to search for tweets!"
 end
