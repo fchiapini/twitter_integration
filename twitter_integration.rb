ea0cf15aa02bbeb73d80a99c19430bbce04501a3
Felipe <fchiapini@gmail.com>
2016-02-18 09:18:38 -0200

    Identation

--- twitter_integration.rb
+++ twitter_integration.rb
@@ -4,12 +4,12 @@ require 'uri'
 require 'json'
 
 if !ARGV.empty?
-	# Connecting to the Twitter Streaming API
- 	tweets = Twitter::Streaming::Client.new do | config |
-	 	config.consumer_key        = "1dB8iOZUUBaFgqozjE2iqfSIg"
-	 	config.consumer_secret     = "jNOQQxmEsJrtxdtnRrMN7JDpDtohKDfSxzQmf9UYOIlemkYQwq"
-	 	config.access_token        = "387129857-fomgij6Mj2CTTbfBcra9AZI4DUccdu3uVhDOEhyT"
-	 	config.access_token_secret = "mSHLJ4elqyoajqTTsaUNW2xdetfYBkAXaxgWHhDt6LAIL"
+  # Connecting to the Twitter Streaming API
+  tweets = Twitter::Streaming::Client.new do | config |
+    config.consumer_key        = "1dB8iOZUUBaFgqozjE2iqfSIg"
+    config.consumer_secret     = "jNOQQxmEsJrtxdtnRrMN7JDpDtohKDfSxzQmf9UYOIlemkYQwq"
+    config.access_token        = "387129857-fomgij6Mj2CTTbfBcra9AZI4DUccdu3uVhDOEhyT"
+    config.access_token_secret = "mSHLJ4elqyoajqTTsaUNW2xdetfYBkAXaxgWHhDt6LAIL"
   end
 
 	# One or multiple keywords
